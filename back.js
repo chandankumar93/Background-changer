e51e245f1afff7cd827c7f82f7b6291fd03fe1e2
chandan kumar <itzchandang@gmail.com>
2026-02-23 23:31:43 +0530

    backgound file

---
+++ back.js
@@ -0,0 +1,27 @@
+let buttons = document.querySelectorAll(".button");
+let body = document.querySelector("body");
+
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(event){
+        
+        if(event.target.id ==='gray'){
+            body.style.backgroundColor= event.target.id;
+        }
+        if(event.target.id ==='red'){
+            body.style.backgroundColor= event.target.id;
+        }
+        if(event.target.id ==='green'){
+            body.style.backgroundColor= event.target.id;
+        }
+        if(event.target.id ==='blue'){
+            body.style.backgroundColor= event.target.id;
+        }
+        if(event.target.id ==='yellow'){
+            body.style.backgroundColor= event.target.id;
+        }
+
+
+    });
+});
+
